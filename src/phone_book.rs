19ad2d5b58b2fff668e94bb4d882ff36ee507809
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-12-16 10:56:27 -0500

    Updated phone_book.rs to add show_operations

--- src/phone_book.rs
+++ src/phone_book.rs
@@ -1,4 +1,22 @@
 use crate::contact::Contact;
+use comfy_table::modifiers::UTF8_ROUND_CORNERS;
+use comfy_table::presets::UTF8_FULL;
+use comfy_table::{Cell, Table};
+
+/**
+ * Define a list of operations available in the phone book.
+ */
+const OPERATIONS: &[(char, &str)] = &[
+    ('C', "Create"),
+    ('Q', "Fuzzy Query"),
+    ('F', "Upload contacts from a CSV file"),
+    ('U', "Update"),
+    ('D', "Delete"),
+    ('E', "Exit"),
+    ('L', "List in original order based on creation time"),
+    ('A', "List in ascending order"),
+    ('Z', "List in descending order"),
+];
 /**
  * Define a PhoneBook struct with a field for a vector of Contact structs.
  */
@@ -34,5 +52,24 @@ impl PhoneBook {
      */
     pub fn add_contact(&mut self, contact: Contact) {
         self.contacts.push(contact);
+    }
+    /**
+     * Display the list of operations available in the phone book as a table.
+     */
+    pub fn show_operations() {
+        let mut table = Table::new();
+        table
+            .load_preset(UTF8_FULL)
+            .apply_modifier(UTF8_ROUND_CORNERS)
+            .set_header(vec![
+                Cell::new("Option").add_attribute(comfy_table::Attribute::Bold),
+                Cell::new("Description").add_attribute(comfy_table::Attribute::Bold),
+            ]);
+
+        for &(option, description) in OPERATIONS {
+            table.add_row(vec![Cell::new(option), Cell::new(description)]);
+        }
+
+        println!("{}", table);
     }
 }
