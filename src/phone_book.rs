b0b6e3526286ea3f272b385791ba276dc177a947
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-12-16 09:36:32 -0500

    Added phone_book.rs

---
+++ src/phone_book.rs
@@ -0,0 +1,41 @@
+use uuid::Uuid;
+
+pub struct PhoneBook {
+    contacts: Vec<Contact>,
+}
+
+impl PhoneBook {
+    pub fn new() -> PhoneBook {
+        PhoneBook {
+            contacts: Vec::new(),
+        }
+    }
+}
+
+pub struct Contact {
+    uuid: Uuid,
+    first_name: String,
+    last_name: String,
+    email: String,
+    address: String,
+    phone_number: String,
+}
+
+impl Contact {
+    pub fn new(
+        first_name: String,
+        last_name: String,
+        email: String,
+        address: String,
+        phone_number: String,
+    ) -> Contact {
+        Contact {
+            uuid: Uuid::new_v4(),
+            first_name,
+            last_name,
+            email,
+            address,
+            phone_number,
+        }
+    }
+}
