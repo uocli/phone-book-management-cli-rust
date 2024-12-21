33df9bf50765dfb80d3fef0c8c003559b9e6430e
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-12-20 19:04:45 -0500

    Updated up.sql to include Contact

--- migrations/2024-12-21-000323_create_contacts/up.sql
+++ migrations/2024-12-21-000323_create_contacts/up.sql
@@ -1,1 +1,10 @@
 -- Your SQL goes here
+CREATE TABLE contacts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT NOT NULL,
+    last_name TEXT,
+    email TEXT,
+    phone TEXT NOT NULL,
+    address TEXT,
+    created_date TEXT
+);
