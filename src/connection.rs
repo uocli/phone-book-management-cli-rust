c157af0602984d2868da098767fb0d06be349e90
Chenyang Li (Sean) <cli049@uottawa.ca>
2024-12-20 19:40:00 -0500

    Updated connection.rs doc

--- src/connection.rs
+++ src/connection.rs
@@ -1,6 +1,15 @@
 use diesel::prelude::*;
 use diesel::sqlite::SqliteConnection;
 
+/// Establishes a connection to the SQLite database using the `DATABASE_URL` environment variable.
+///
+/// # Panics
+///
+/// This function will panic if the `DATABASE_URL` environment variable is not set.
+///
+/// # Returns
+///
+/// Returns a `SqliteConnection` instance representing the established connection to the database.
 pub fn establish_connection() -> SqliteConnection {
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     SqliteConnection::establish(&database_url)
